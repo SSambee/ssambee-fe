4149bce7d4f45938b5ff98c5f27bff1f3b292b2a
p-changki <dnsxj12345aa@gmail.com>
2026-02-05 17:05:48 +0900

    feat: match sidebar and header sizes

--- src/components/common/sidebar/AppSidebar.tsx
+++ src/components/common/sidebar/AppSidebar.tsx
@@ -116,7 +116,7 @@ export function AppSidebar() {
 
   return (
     <Sidebar className="border-r border-[#e9ebf0] [&_[data-slot=sidebar-inner]]:bg-white">
-      <SidebarHeader className="px-10 py-8">
+      <SidebarHeader className="pl-10 pr-8 py-8">
         <span className="text-[24px] leading-[32px] tracking-[-0.24px] font-bold text-black">
           Logo
         </span>
